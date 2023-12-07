9074ada355cca4f01367b2d1593968140b873266
nisch <nischay17p@gmail.com>
2023-12-07 23:02:49 +0530

    commit

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from src.mlProject import logger
 logger.info("Welcome man!")
+
+
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>> stage{STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>>stage{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
